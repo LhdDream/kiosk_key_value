11ca978ac297ac933f12dcc8a348e415cb8c163c
LhdDream <626487728@qq.com>
2019-10-07 17:25:13 +0800

    update sstable

--- my_redies/src/sstable.h
+++ my_redies/src/sstable.h
@@ -11,58 +11,54 @@
 #include <algorithm>
 #include "../base/sds.h"
 #include "../util/bloom_filter.h"
-#include "../util/parallel_hashmap/phmap.h"
 #include <snappy.h>
-using phmap::parallel_flat_hash_map;
+//
+//fifter
+//block
+//总目录
+//静态索引一层，数据一层
 class sstable{
 public:
-    explicit sstable() : ht_(std::make_unique<parallel_flat_hash_map<sds,sds,HashFunc,EqualKey>>()){
-
-    };
+    sstable() = default;
     ~sstable() = default;
-    void save(std::unique_ptr<bloom> & bloom_){
-         std::string name(std::to_string(id) + ".tss");
-         bloom_->write_(name);
-         std::string buf;//作为一个标志
-         append_(buf);
-         std::string write__;
-         snappy::Compress(buf.data(),buf.size(),&write__);
-         size = write__.size();
-         write__ += std::to_string(size) + write__;
-         write_(name,write__);
-         id++;
+    void add(std::string & key,sds &value)
+    {
+        recode_.emplace_back(key,value);
     }
-    void swap(std::unique_ptr< parallel_flat_hash_map<sds,sds,HashFunc,EqualKey> > &ht){
-        ht_.swap(ht);
-    }
-    void append_(std::string & buf){
-        for(auto &c : *ht_){
-            buf.append(c.first.Tostring());
-            buf.append(c.second.Tostring());
-        }
-    }
-    void write_(std::string &filename,std::string &buf){
-        int fd = open(filename.c_str(),O_APPEND | O_WRONLY | O_CREAT ,S_IRUSR | S_IWUSR );
-        struct stat sb{};
-        if((fstat(fd,&sb)) == -1)
-        {
-            return ;
-        }
-        if((ftruncate(fd,1024 + size) )== -1) //使用mmap之前必须扩大文件大小，因为mmap写入不能大于文件大小
-        {
-            return ;
-        }
-        char *memPtr;
-        memPtr = (char *)mmap(nullptr, 1024 + size , PROT_READ | PROT_WRITE,
-                              MAP_SHARED, fd, 0);
-        memmove(memPtr,buf.c_str(), buf.size()); // 写入1KB的fifter
-        msync(memPtr,buf.size(),0);
-        munmap(memPtr,buf.size());
-        close(fd);
+    void bloomadd(std::unique_ptr<bloom> &&bl){
+        bloom_.emplace_back(std::move(bl));
     }
 private:
-    std::unique_ptr< parallel_flat_hash_map<sds,sds,HashFunc,EqualKey> >  ht_;
-    unsigned  long long id  = 0; // 标记打开是第几块的sstable
-    unsigned  long long size = 0;
+    struct fifter{
+        uint32_t  begin;
+        uint32_t  offest; //每一块过滤的起始位置和偏移量
+    };
+    std::vector<fifter> fifter_ ; //布隆过滤器的总目录
+    struct index{
+       sds key_; // 每一块最后的key
+       uint32_t  offest; // 每一个数据块的偏移位置
+    };
+    std::vector<index> index_;//生成一块静态索引
+    std::vector<index> restarte_; // 重启点
+    //重启点
+    struct recode{
+    private:
+         uint16_t key_length;
+         std::string key;
+         uint32_t value_length;
+         sds value;
+    public:
+         bool operator < (recode & rhs){
+             return this->key < rhs.key;
+         }
+         recode(std::string & key_ , sds &value_) : key_length(key_.size()),
+         key(key_),value_length(value_.size()),value(value_){
+         }
+         ~recode() = default;
+    };
+    std::vector<recode> recode_; //数据块
+    //总的过滤器 延长生命周期
+    std::vector<std::unique_ptr<bloom>> bloom_;
 };
+
 #endif //MY_REDIES_SSTABLE_H
